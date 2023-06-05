09e43d209923fd2c597cc44041e40797313bc1c5
ActuaryEmma <emmanyakio@gmail.com>
2023-06-05 23:04:51 +0300

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,7 +6,7 @@ int check_cycle(listint_t *list)
 	/* used to traverse and check for a cycle */
 	listint_t *list2 = list;
 
-	while (list != NULL)
+	while (list != NULL && list->next != NULL)
 	{
 		list2 = list2->next;
 		list = list->next->next;
