00c27a07ce86c9022938ecb7b8e47ee2099cfa4d
ActuaryEmma <emmanyakio@gmail.com>
2023-06-06 23:05:46 +0300

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(90, 64, -1):
     if (i % 2 == 0):
         print("{}".format(chr(i + 32)), end="")
     else:
-         print("{}".format(chr(i)), end="")
+        print("{}".format(chr(i)), end="")
