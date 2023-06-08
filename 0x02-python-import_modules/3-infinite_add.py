de005df704b98c60fc16579937bc446085a48328
ActuaryEmma <emmanyakio@gmail.com>
2023-06-08 16:42:50 +0300

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
-total = 0
-length = len(sys.argv)
-# if only one argument or no argument
-if (length <= 1):
-    print("0")
-else:
-    for i in range(1, length):
-        for j in range(len(sys.argv[i])):
-            # if not a digit
-            if not sys.argv[i][j].isdigit():
-                print("Error")
-                sys.exit()
-        # add the integer in the command line
-        total += int(sys.argv[i])
-    print("{}".format(total))
+if __name__ == '__main__':
+    import sys
+    total = 0
+    length = len(sys.argv)
+    # if only one argument or no argument
+    if (length <= 1):
+        print("0")
+    else:
+        for i in range(1, length):
+            for j in range(len(sys.argv[i])):
+                # if not a digit
+                if not sys.argv[i][j].isdigit():
+                    print("Error")
+                    sys.exit()
+            # add the integer in the command line
+            total += int(sys.argv[i])
+        print("{}".format(total))
