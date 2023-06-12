2390c73df64cd077723704dbb458e42b07b50283
ActuaryEmma <emmanyakio@gmail.com>
2023-06-12 23:48:49 +0300

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,7 @@
 #include "lists.h"
 /**
   * is_palindrome - list that is the same from left to right and vice versa
+  * split the linked list in the middle
   * @head: pointer to the head
   * Return: 0 if not palindrome, 1 if palindrome
   */
@@ -10,10 +11,50 @@ void reverse_list(listint_t **head);
 int compareLists(listint_t *list1, listint_t *list2);
 int is_palindrome(listint_t **head)
 {
-		listint_t *reversed = NULL;
-		reverse_list(head);
-		reversed = *head;
-		return (compareLists(*head, reversed));
+		   listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *temp = NULL;
+    int result = 1;
+
+    if (*head == NULL || (*head)->next == NULL)
+        return (result);
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        prev = slow;
+        slow = slow->next;
+    }
+
+    if (fast != NULL)
+    {
+        slow = slow->next;
+    }
+
+    temp = slow;
+    reverse_list(&temp);
+    *head = temp;
+
+    while (temp != NULL)
+    {
+        if ((*head)->n != temp->n)
+        {
+            result = 0;
+            break;
+        }
+        *head = (*head)->next;
+        temp = temp->next;
+    }
+
+    reverse_list(head);
+
+    if (prev != NULL)
+        prev->next = slow;
+    else
+        *head = slow;
+
+    return (result);
 }
 
 void reverse_list(listint_t **head)
