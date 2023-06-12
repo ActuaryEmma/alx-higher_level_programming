743177898fe95da2494b6e132edcd83dfd3063b1
ActuaryEmma <emmanyakio@gmail.com>
2023-06-12 14:40:09 +0300

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    for word in range(0, length):
-        if (sentence == ""):
-            letter = None
-        else:
-            letter = sentence[0]
+    if (sentence == ""):
+        letter = None
+    else:
+        letter = sentence[0]
     return (length, letter)
