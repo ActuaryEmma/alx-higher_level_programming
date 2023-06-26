235c55ad24ed7cef6eb9e07bf66386bb9e201545
ActuaryEmma <emmanyakio@gmail.com>
2023-06-27 02:17:15 +0300

    divide two integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return (result)
-    except ZeroDivisionError:
+    except (ZeroDivisionError, NameError, TypeError, SyntaxError):
         return (None)
     finally:
         print("Inside result: {}".format(result))
