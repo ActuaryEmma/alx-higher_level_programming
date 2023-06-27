e6a4b4c00b7a6cec980f42f4e83ec4e862f29b02
ActuaryEmma <emmanyakio@gmail.com>
2023-06-27 23:32:54 +0300

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,6 +7,11 @@ size must be an int else raise an error
 
 class Square:
     """Represents a Sqaure with a size"""
-    def __init__(self, size=0):
-        """Initialize the size"""
-        self.__size = size
+    try:
+        def __init__(self, size=0):
+            """Initialize the size"""
+            self.__size = size
+    except TypeError:
+        print("size must be an integer")
+    except ValueError:
+        print("size must be >= 0")
