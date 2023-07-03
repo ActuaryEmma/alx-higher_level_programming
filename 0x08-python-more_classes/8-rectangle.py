b252fee7d16be916928600a79458d36066153ba9
ActuaryEmma <emmanyakio@gmail.com>
2023-07-03 20:44:21 +0300

    added error message in init function

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -15,6 +15,15 @@ class Rectangle:
         """
         Initialize width and height
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = height
         self.__width = width
         Rectangle.number_of_instances += 1
