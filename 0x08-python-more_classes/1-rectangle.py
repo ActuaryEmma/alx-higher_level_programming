1ec13e1e5f84a68d038b0b6add52163a01279967
ActuaryEmma <emmanyakio@gmail.com>
2023-07-03 14:08:11 +0300

    replaces return with raise

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,9 +24,9 @@ class Rectangle:
     def width(self, value):
         """setter"""
         if type(value) is not int:
-            return TypeError("width must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            return ValueError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -38,7 +38,7 @@ class Rectangle:
     def height(self, value):
         """setter"""
         if type(value) is not int:
-            return TypeError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            return ValueError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
