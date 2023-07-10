6dbf239ff53799fb2a50fc950283710882c852e5
ActuaryEmma <emmanyakio@gmail.com>
2023-07-10 18:51:29 +0300

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+no moule used
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    if the object is an instance of, if the object is an
+    instance of a class that inherited from, the specified class
+    """
+    return isinstance(obj, a_class)
