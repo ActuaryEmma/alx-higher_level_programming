169f718ffa390612910ac2b12cb46e6122412e58
ActuaryEmma <emmanyakio@gmail.com>
2023-07-11 00:12:38 +0300

    Task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+module BaseGeomety is the parent class
+"""
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle that inherits from BaseGoemetry """
+
+    def __init__(self, width, height):
+        """instantiate width and height"""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area of a rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ string representation of the rectangle """
+        return f"[Rectangle] {self.__width}/{self.__height}"
