8ae03bd7354337a874280e5ffef0ee4533b8743f
ActuaryEmma <emmanyakio@gmail.com>
2023-07-11 22:59:29 +0300

    function documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,6 @@ no module used
 
 
 def read_file(filename=""):
+    """ function that reads and open a file"""
     with open(filename, encoding="utf-8") as f:
         print("{}".format(f.read()), end="")
