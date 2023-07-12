10fabca670088a49845b0445cca093e7584bbd1b
ActuaryEmma <emmanyakio@gmail.com>
2023-07-12 09:31:50 +0300

    removed try expect

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,9 +7,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file"""
-    try:
-        with open(filename, "w") as f:
-            json_str = json.dumps(my_obj)
-            f.write(json_str)
-    except TypeError:
-        print("[TypeError] {} is not JSON serializable".format(my_obj))
+    with open(filename, "w") as f:
+        json_str = json.dumps(my_obj)
+        f.write(json_str)
