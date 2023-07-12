db36acccbafe12b719d3bc67ea16fdc1c9ab8332
ActuaryEmma <emmanyakio@gmail.com>
2023-07-12 12:19:25 +0300

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+no module imported
+"""
+
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        """instantiate public attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if (attrs is None):
+            return self.__dict__
+        dic = {}
+        for key, value in self.__dict__.items():
+            for i in attrs:
+                if key == i:
+                    dic[key] = value
+        return dic
