5fb5c2b1a2cbad87a78f8a2b7cc7acdf4ebdb762
ActuaryEmma <emmanyakio@gmail.com>
2023-07-12 13:21:35 +0300

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""no module"""
+
+
+if __name__ == "__main__":
+
+    def pascal_triangle(n):
+        """function"""
+        list1 = []
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                list1.append(j)
+        return list1
