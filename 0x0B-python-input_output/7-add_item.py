4161e69c1b110202a28588823aa86e3429087a98
ActuaryEmma <emmanyakio@gmail.com>
2023-07-12 12:43:50 +0300

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,8 @@ module used from different files
 """
 from sys import argv
 from json import JSONDecodeError
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 list1 = []
