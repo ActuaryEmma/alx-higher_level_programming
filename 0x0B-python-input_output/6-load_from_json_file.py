06786e9cda4b31e0347d745b1cc42ea3e36470a3
ActuaryEmma <emmanyakio@gmail.com>
2023-07-12 10:39:08 +0300

    remove try, except

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,9 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """convert JSON string to object"""
-    try:
-        with open(filename) as f:
-            new_obj = json.load(f)
-    except ValueError:
-        new_obj = None
-    return new_obj
+    with open(filename) as f:
+        json.load(f)
