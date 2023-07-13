27ae35d8ccc8239ed4981581f3a0eb312195323e
ActuaryEmma <emmanyakio@gmail.com>
2023-07-14 00:57:57 +0300

    Task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+no module
+"""
+
+
+
 class LockedClass:
+    """class Lockedclass"""
     def __setattr__(self, attr, value):
+        """initializer"""
         if not hasattr(self, attr) and attr != 'first_name':
             raise AttributeError(f"'{self.__class__.__name__}' object\
 has no attribute '{attr}'")
