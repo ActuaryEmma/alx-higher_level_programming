814df9a080f89ffb93ae72270570dd8cb91d8089
ActuaryEmma <emmanyakio@gmail.com>
2023-07-15 23:53:20 +0300

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 no module
 """
+import json
 
 
 class Base():
@@ -15,3 +16,9 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """return Json string representation"""
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
