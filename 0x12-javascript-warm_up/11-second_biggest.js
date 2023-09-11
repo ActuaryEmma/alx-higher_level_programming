cbb7f9b2f11f1d9b45253a00da9dddcc13b176b1
ActuaryEmma <emmanyakio@gmail.com>
2023-09-12 00:32:44 +0300

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // searches biggest integer in the list of arguments
 
-const num = process.argv.length - 2;
-if (isNaN(num < 2)) {
+const num = process.argv.length;
+if (num <= 3) {
   console.log(0);
 } else {
   const list = process.argv.sort();
