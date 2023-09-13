a47c55d4a66eac01c04f9b4a7b7e7030df7917cd
ActuaryEmma <emmanyakio@gmail.com>
2023-09-14 00:15:33 +0300

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// defines a square and inherits from Rectangle
+
+class Square extends require('./5-square.js') {
+  charPrint (C) {
+    if (C === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'C';
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+module.exports = Square;
