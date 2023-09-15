0277aa97a23320d6a9455194a45793e5e65200cd
ActuaryEmma <emmanyakio@gmail.com>
2023-09-15 23:33:44 +0300

    Task 0 pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,9 @@
      lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", port=3306, user="emma", passwd="emma", db="hbtn_0e_0_usa");
+db = MySQLdb.connect(host="localhost",
+                     port=3306,
+                     user="emma", passwd="emma", db="hbtn_0e_0_usa")
 
 # prepare cursor object
 cursor = db.cursor()
