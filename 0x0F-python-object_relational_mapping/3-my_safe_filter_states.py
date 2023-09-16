c0134ca1773987817cf8fb736f5237b9a817d2d5
ActuaryEmma <emmanyakio@gmail.com>
2023-09-16 09:19:12 +0300

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" MySQLdb provides a way db interact with python scripts
+     lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) != 5:
+        print("Error")
+        exit(1)
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    stName = argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    # prepare cursor object
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+
+    try:
+        cursor.execute(sql, (stName,))
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error executing the SELECT quesry: {e}")
+    db.close()
