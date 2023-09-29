0fed7b1c0a2d27102eb05be9b9dc17f73a5ef8c0
ActuaryEmma <emmanyakio@gmail.com>
2023-09-29 22:40:54 +0300

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+requests- interact with web services, API or websites by making HTTP requests
+requests.get : retrive content of a web page
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email' : email}
+    try:
+        with requests.post(url, data=data) as response:
+            response.raise_for_status()
+            html = response.text
+        print(html)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
