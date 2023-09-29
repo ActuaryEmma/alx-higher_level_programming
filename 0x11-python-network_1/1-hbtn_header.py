47a4e6505c72778528fc1c5d9eab7ccf3f9beb1e
ActuaryEmma <emmanyakio@gmail.com>
2023-09-29 20:31:59 +0300

    Task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+with : ensure the response is closed when done
+response.headers: contains dictionary like object that contains the headers
+.get: retrives the value associated with the key "X-Request-Id"
+"""
+
+
+req = urllib.request.Request(sys.argv[1])
+with urllib.request.urlopen(req) as response:
+    print(response.headers.get("X-Request-Id"))
