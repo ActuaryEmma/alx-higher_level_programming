7525e0166a0497c752095ca6508f4b42019b0da3
ActuaryEmma <emmanyakio@gmail.com>
2023-09-29 22:21:50 +0300

    Task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+requests- interact with web services, API or websites by making HTTP requests
+requests.get : retrive content of a web page
+"""
+import requests
+import sys
+
+
+url = sys.argv[1]
+try:
+    with requests.get(url) as response:
+        response.raise_for_status()
+        print(response.headers.get('X-Request-Id'))
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
