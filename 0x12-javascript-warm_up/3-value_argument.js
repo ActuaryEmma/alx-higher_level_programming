964cf5ead5bcf1dacdf350bc27e6a4ff6f59bb94
ActuaryEmma <emmanyakio@gmail.com>
2023-10-01 14:29:37 +0300

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 // prints the first argument passed to it
 
-if (process.argv[2] === 'undefined') {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
